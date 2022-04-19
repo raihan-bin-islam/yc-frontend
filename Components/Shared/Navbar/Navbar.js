ea45f3a85dd7c2f2d5379fdf2b2b650f68047e3f
farhan21 <farhanabsar21@gmail.com>
2022-04-19 16:16:09 +0600

    navbar

--- Components/Shared/Navbar/Navbar.js
+++ Components/Shared/Navbar/Navbar.js
@@ -3,11 +3,15 @@ import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import styles from './Navbar.module.scss';
 import logo from '../../../public/yunus_center.png'
-
+import magnifyIcon from '../../../public/icon_magnify.svg'
+import burgerIcon from '../../../public/icon_burger.svg'
+import crossIcon from '../../../public/icon_cross.svg'
+import { useRouter } from 'next/router';
 
 const Navbar = () => {
-    const { nav, navContainer, navScroll } = styles
+    const { nav, navContainer, navScroll, navActive, dropdown, mainList, burger, cross } = styles
     const [scrolled, setScrolled] = useState(false)
+    const router = useRouter();
 
     const handleScroll = () => {
 
@@ -34,17 +38,35 @@ const Navbar = () => {
                     <img src={logo.src} alt='brand-yc'/>
                 </div>
                 <div>
-                    <ul>
-                        <li><Link href='/'>Home</Link></li>
-                        <li><Link href='/'>About</Link></li>
+                    <ul className={mainList}>
+                        <li>
+                            <Link href='/'>
+                                <a className={router.pathname == "/" ? navActive : ""}>
+                                    Home
+                                </a>
+                            </Link>
+                        </li>
+                        <li>About
+                            <ul className={dropdown}>
+                                <li>About Us</li>
+                                <li>About Us</li>
+                                <li>About Us</li>
+                                <li>About Us</li>
+                            </ul>
+                        </li>
                         <li><Link href='/'>Social Business</Link></li>
                         <li><Link href='/'>Visit Programs</Link></li>
                         <li><Link href='/'>Publications</Link></li>
                         <li><Link href='/'>Media</Link></li>
                         <li><Link href='/'>YSBC</Link></li>
                         <li><Link href='/'>Prof. Yunus</Link></li>
+                        <li><img src={magnifyIcon.src} alt='brand-yc'/></li>
                     </ul>
                 </div>
+            </div>
+            <div>
+                <img src={burgerIcon.src} alt='brand-yc' className={burger}/>
+                <img src={crossIcon.src} alt='brand-yc' className={cross}/>
             </div>
         </nav>
     );
