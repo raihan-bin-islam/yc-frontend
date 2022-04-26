8b86d75135662e0e43f66b9d276f03a6ba910737
raihan <rishaon0@gmail.com>
2022-04-26 16:02:34 +0600

    slider card added for awards section

--- pages/component.js
+++ pages/component.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Heading from "../Components/Shared/Heading/Heading";
 import Highlight from "../Components/Shared/Highlight/Highlight";
 import NewsCard from "../Components/Shared/NewsCard/NewsCard";
 import SliderCard from "../Components/Shared/SliderCard/SliderCard";
@@ -8,15 +7,19 @@ import WhatWeDoCard from "../Components/Shared/WhatWeDoCard/WhatWeDoCard";
 const Component = () => {
   return (
     <>
-      <Highlight />
+      {/* <Highlight /> */}
       {/* <WhatWeDoCard /> */}
-      <NewsCard />
-      <div className="grid" style={{ display: "grid", gridTemplateColumns: "repeat(4,1fr)", gap: 50 }}>
-        <SliderCard type="articles" />
-        <SliderCard type="articles" size={2} />
-        <SliderCard type="articles" />
+      {/* <NewsCard /> */}
+      <div className="grid" style={{ display: "grid", gridTemplateColumns: "repeat(4,1fr)", gap: 0 }}>
+        <SliderCard image="/assets/images/landing_page/initiative1.jpg" type="awards" />
+        <SliderCard image="/assets/images/landing_page/initiative1.jpg" type="awards" />
+        <SliderCard image="/assets/images/landing_page/initiative1.jpg" type="awards" />
+        <SliderCard image="/assets/images/landing_page/initiative1.jpg" type="awards" />
+        <SliderCard image="/assets/images/landing_page/initiative1.jpg" type="awards" />
+        <SliderCard image="/assets/images/landing_page/initiative1.jpg" type="awards" />
+        <SliderCard image="/assets/images/landing_page/initiative1.jpg" type="awards" />
+        <SliderCard image="/assets/images/landing_page/initiative1.jpg" type="awards" />
       </div>
-      <Heading text="What We Do" size="large" />
     </>
   );
 };
