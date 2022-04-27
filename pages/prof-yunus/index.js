e0f9e9c7e122d0dd82edb1b37ffe06be6567aa29
farhan21 <farhanabsar21@gmail.com>
2022-04-27 15:56:03 +0600

    books section

--- pages/prof-yunus/index.js
+++ pages/prof-yunus/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Hero from "../../Components/ProfYunus/Hero/Hero";
+import ProfBooks from "../../Components/ProfYunus/ProfBooks/ProfBooks";
 import ProfPioneer from "../../Components/ProfYunus/ProfPioneer/ProfPioneer";
 
 const ProfYunus = () => {
@@ -7,6 +8,7 @@ const ProfYunus = () => {
     <div>
       <Hero />
       <ProfPioneer/>
+      <ProfBooks/>
     </div>
   );
 };
