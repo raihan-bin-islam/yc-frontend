6d33b1d6d4d1feb932d123f2bd8cf8b8d666ffda
farhan21 <farhanabsar21@gmail.com>
2022-05-09 17:58:23 +0600

    fixes

--- Components/Home/HomeHeader/HomeHeader.js
+++ Components/Home/HomeHeader/HomeHeader.js
@@ -3,6 +3,10 @@ import styles from "./homeheader.module.scss";
 import heroImage from "../../../public/assets/images/landing_page/hero_path.png";
 import heroYunus from "../../../public/assets/images/landing_page/hero_banner.webp";
 
+// typewriter
+import Typewriter from 'typewriter-effect';
+
+//component
 const HomeHeader = () => {
   const { headerHero, heroBanner, heroBody, heroContentLeft, heroContentRight, h2 } = styles;
   return (
@@ -19,10 +23,19 @@ const HomeHeader = () => {
           <h6>2006 Nobel Peace Laureate</h6>
         </div>
         <div className={heroContentRight}>
-          <span className={h2}>“If you imagine,</span>
-          <span className={h2}>someday it will happen;</span>
-          <span className={h2}>if you don’t imagine,</span>
-          <span className={h2}>it will never happen.”</span>
+          {/* <span className={h2}>“If you imagine, someday it will happen; if you don’t imagine, it will never happen.”</span> */}
+          <span className={h2}>
+            <Typewriter
+              onInit={(typewriter) => {
+                typewriter.typeString('“If you imagine, someday it will happen; if you don’t imagine, it will never happen.”')
+                  .start()
+              }}
+              options={{
+                autoStart: true,
+                delay: 75
+              }}
+            />
+          </span>
         </div>
       </div>
     </section>
