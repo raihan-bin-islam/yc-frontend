ac2cd0899f25cb6dcb1628ccd5006ae9c7df0dba
farhan21 <farhanabsar21@gmail.com>
2022-05-11 17:34:00 +0600

    scroll button

--- Components/Shared/Layout/Layout.js
+++ Components/Shared/Layout/Layout.js
@@ -1,9 +1,38 @@
 import Head from "next/head";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Footer from "../Footer/Footer";
 import Navbar from "../Navbar/Navbar";
+import arrowUp from '../../../public/arrow_up.svg'
+import arrowDown from '../../../public/arrow_down.svg'
 
 const Layout = ({ children }) => {
+
+  const [showUp, setShowUp] = useState(false)
+  const [showDown, setShowDown] = useState(true)
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth' // for smoothly scrolling
+    });
+  };
+
+  const scrollDown = () => {
+    window.scrollTo(0, 1000);
+  }
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 300) {
+        setShowUp(true)
+        setShowDown(false)
+      } else {
+        setShowUp(false)
+        setShowDown(true)
+      }
+    });
+  }, []);
+
   return (
     <>
       <Head>
@@ -28,8 +57,32 @@ const Layout = ({ children }) => {
         />
       </Head>
       <Navbar />
+      {/* scroll buttons */}
+      {showUp && <button className="scroll_btn_up" onClick={scrollToTop}>
+        <img src={arrowUp.src} alt='arrow-up'/>
+      </button>}
+      {showDown && <button className="scroll_btn_down" onClick={scrollDown}>
+        <img src={arrowDown.src} alt='arrow-up'/>
+      </button>}
+      {/* children */}
       {children}
       <Footer />
+      <style jsx>{`
+        .scroll_btn_up {
+          position: fixed;
+          right: 2.5%;
+          top: 95vh;
+          z-index: 1000;
+          background-color: inherit;
+        }
+        .scroll_btn_down {
+          position: fixed;
+          right: 2.5%;
+          top: 95vh;
+          z-index: 1000;
+          background-color: inherit;
+        }
+      `}</style>
     </>
   );
 };
