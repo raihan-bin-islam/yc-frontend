652b7074c870259ddd137f4e0657c6c4a4189307
raihan <rishaon0@gmail.com>
2022-05-12 16:13:38 +0600

    api integration added to page level

--- pages/index.js
+++ pages/index.js
@@ -6,16 +6,23 @@ import HomeNews from "../Components/Home/HomeNews/HomeNews";
 import HomeVideoSlide from "../Components/Home/HomeVideoSlide/HomeVideoSlide";
 import HomeWhatWeDo from "../Components/Home/HomeWhatWeDo/HomeWhatWeDo";
 
+import useFetch from "../Components/Hooks/useFetch";
+
 export default function Home() {
+  const [eventIsLoading, latestEvents] = useFetch("/events");
+  const [newsIsLoading, newsData] = useFetch("/news");
+  const [articlesIsPending, articlesData] = useFetch("/articles");
+  const [videoIsLoading, videoData] = useFetch("/yunus-speech");
+
   return (
     <div>
       <HomeHeader />
       <HomeWhatWeDo />
-      <HomeLatestEvents />
-      <HomeNews />
-      <HomeArticles />
+      {latestEvents.length > 0 && <HomeLatestEvents isLoading={eventIsLoading} latestEvents={latestEvents} />}
+      {newsData.length > 0 && <HomeNews isLoading={newsIsLoading} newsData={newsData} />}
+      {articlesData.length > 0 && <HomeArticles isLoading={articlesIsPending} articlesData={articlesData} />}
       <HomeInitiatives />
-      <HomeVideoSlide />
+      {videoData.length > 0 && <HomeVideoSlide isLoading={videoIsLoading} videoData={videoData} />}
     </div>
   );
 }
