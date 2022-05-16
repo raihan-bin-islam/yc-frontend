1b9d2ec0ece83ad4b16a8858740dd5d54a0e6691
Khandaker Nabiul <na.ninad60@gmail.com>
2022-05-16 18:32:51 +0600

    social business concept page complete with responsive, heroBanner responsive issue fixed

--- pages/social-business-concept/index.js
+++ pages/social-business-concept/index.js
@@ -2,11 +2,13 @@ import React from "react";
 
 // COMPONENTS
 import SocialBusinessConceptHeader from "../../Components/SocialBusinessConcept/SocialBusinessConceptHeader/SocialBusinessConceptHeader";
+import SocialBusinessConceptBody from "../../Components/SocialBusinessConcept/SocialBusinessConceptBody/SocialBusinessConceptBody";
 
 const SocialBusinessConcept = () => {
   return (
     <div>
       <SocialBusinessConceptHeader />
+      <SocialBusinessConceptBody />
     </div>
   );
 };
