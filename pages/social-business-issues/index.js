e1790d677b7c72755530b181ce440df1dc5b4d83
Khandaker Nabiul <na.ninad60@gmail.com>
2022-05-17 18:21:42 +0600

    social business issues body component created and with responsive

--- pages/social-business-issues/index.js
+++ pages/social-business-issues/index.js
@@ -2,11 +2,13 @@ import React from "react";
 
 // COMPONENTS
 import SocialBusinessIssuesHeader from "../../Components/SocialBusinessIssues/SocialBusinessIssuesHeader/SocialBusinessIssuesHeader";
+import SocialBusinessIssuesBody from "../../Components/SocialBusinessIssues/SocialBusinessIssuesBody/SocialBusinessIssuesBody";
 
 const SocialBusinessIssues = () => {
   return (
     <div>
       <SocialBusinessIssuesHeader />
+      <SocialBusinessIssuesBody />
     </div>
   );
 };
