37b4a53656bcbc8e9b16f62b8d074eb6e174afc1
Khandaker Nabiul <na.ninad60@gmail.com>
2022-05-18 18:29:34 +0600

    social business fag body component added

--- pages/social-business-faq/index.js
+++ pages/social-business-faq/index.js
@@ -2,11 +2,13 @@ import React from "react";
 
 // COMPONENTS
 import SocialBusinessFaqHeader from "../../Components/SocialBusinessFaq/SocialBusinessFaqHeader/SocialBusinessFaqHeader";
+import SocialBusinessFaqBody from "../../Components/SocialBusinessFaq/SocialBusinessFaqBody/SocialBusinessFaqBody";
 
 const index = () => {
   return (
     <div>
       <SocialBusinessFaqHeader />
+      <SocialBusinessFaqBody />
     </div>
   );
 };
