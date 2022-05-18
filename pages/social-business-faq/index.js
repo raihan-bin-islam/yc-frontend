a2de8824c3ee8160cace2ffe85896d476f8ea6eb
Khandaker Nabiul <na.ninad60@gmail.com>
2022-05-18 16:25:12 +0600

    Social business faq page created and related image added in asset

---
+++ pages/social-business-faq/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// COMPONENTS
+import SocialBusinessFaqHeader from "../../Components/SocialBusinessFaq/SocialBusinessFaqHeader/SocialBusinessFaqHeader";
+
+const index = () => {
+  return (
+    <div>
+      <SocialBusinessFaqHeader />
+    </div>
+  );
+};
+
+export default index;
