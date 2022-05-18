265b7ef4c3b2f762d0976da38d84b034f4b05d67
raihan <rishaon0@gmail.com>
2022-05-18 18:18:37 +0600

    prof yunus animation fixed

--- Components/ProfYunus/Awards/awardsData.js
+++ Components/ProfYunus/Awards/awardsData.js
@@ -1,41 +1,49 @@
 export const awardsData = [
   // 1st 8
   {
+    id: 0,
     image: "nobel_peace_prize.png",
     title: "Nobel Peace Prize",
     year: "2016",
   },
   {
+    id: 1,
     image: "olympic_laurel.png",
     title: "Olympic Laurel",
     year: "2021",
   },
   {
+    id: 2,
     image: "presidential_medal.png",
     title: "Presidential Medal of Freedom",
     year: "2009",
   },
   {
+    id: 3,
     image: "gold_medal.png",
     title: "The Congressional Gold Medal",
     year: "2013",
   },
   {
+    id: 4,
     image: "notre_dame.png",
     title: "Notre Dame Award",
     year: "2009",
   },
   {
+    id: 5,
     image: "lamp_of_peace.png",
     title: "Lamp of Peace of Saint Francis",
     year: "2019",
   },
   {
+    id: 6,
     image: "george_washington.png",
     title: "George Washington University President’s Medal",
     year: "2016",
   },
   {
+    id: 7,
     image: "supreme_award.png",
     title: "The Supreme Award of IEPF",
     year: "2016",
@@ -43,41 +51,49 @@ export const awardsData = [
   // 2nd 8
 
   {
+    id: 8,
     image: "olympic_laurel.png",
     title: "Olympic Laurel",
     year: "2021",
   },
   {
+    id: 9,
     image: "supreme_award.png",
     title: "The Supreme Award of IEPF",
     year: "2016",
   },
   {
+    id: 10,
     image: "nobel_peace_prize.png",
     title: "Nobel Peace Prize",
     year: "2016",
   },
   {
+    id: 11,
     image: "presidential_medal.png",
     title: "Presidential Medal of Freedom",
     year: "2009",
   },
   {
+    id: 12,
     image: "gold_medal.png",
     title: "The Congressional Gold Medal",
     year: "2013",
   },
   {
+    id: 13,
     image: "george_washington.png",
     title: "George Washington University President’s Medal",
     year: "2016",
   },
   {
+    id: 14,
     image: "notre_dame.png",
     title: "Notre Dame Award",
     year: "2009",
   },
   {
+    id: 15,
     image: "lamp_of_peace.png",
     title: "Lamp of Peace of Saint Francis",
     year: "2019",
@@ -86,42 +102,50 @@ export const awardsData = [
   // 3rd 8
 
   {
+    id: 16,
     image: "olympic_laurel.png",
     title: "Olympic Laurel",
     year: "2021",
   },
   {
+    id: 17,
     image: "presidential_medal.png",
     title: "Presidential Medal of Freedom",
     year: "2009",
   },
   {
+    id: 18,
     image: "gold_medal.png",
     title: "The Congressional Gold Medal",
     year: "2013",
   },
   {
+    id: 19,
     image: "george_washington.png",
     title: "George Washington University President’s Medal",
     year: "2016",
   },
   {
+    id: 20,
     image: "notre_dame.png",
     title: "Notre Dame Award",
     year: "2009",
   },
   {
+    id: 21,
     image: "lamp_of_peace.png",
     title: "Lamp of Peace of Saint Francis",
     year: "2019",
   },
 
   {
+    id: 22,
     image: "nobel_peace_prize.png",
     title: "Nobel Peace Prize",
     year: "2016",
   },
   {
+    id: 23,
     image: "supreme_award.png",
     title: "The Supreme Award of IEPF",
     year: "2016",
--- Components/Shared/AwardsCard/AwardsCard.js
+++ Components/Shared/AwardsCard/AwardsCard.js
@@ -3,23 +3,20 @@ import React, { useState, useEffect } from "react";
 import styles from "./award_card.module.scss";
 import { animated, useSpring } from "@react-spring/web";
 
-const AwardsCard = ({ className, image, title, year, i, animate = false, style }) => {
+const AwardsCard = ({ className, image, title, year, i }) => {
   const { awardCardContainer, titleContainer, cardTitle, cardImg, yearStyles } = styles;
-  const [animation, setAnimation] = useState(animate);
-  useEffect(() => {
-    setAnimation(true);
-  }, []);
 
   const animationStyle = useSpring({
     from: { opacity: 0 },
     to: { opacity: 1 },
-    delay: i * 200,
+    delay: i * 500,
     config: { duration: 1000 },
   });
+
   return (
     <>
-      <animated.div style={style} className={`${awardCardContainer} ${className}`}>
-        <img className={cardImg} src={image} alt="" />
+      <animated.div style={animationStyle} className={`${awardCardContainer} ${className}`}>
+        <img className={cardImg} src={image} alt={title} />
         <div className={titleContainer}>
           <h4 className={cardTitle}>{title}</h4>
           <h4 className={yearStyles}>{year}</h4>
