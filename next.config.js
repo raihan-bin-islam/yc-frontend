8705232ae51c51fa6a3c933fa73e55856b95f90c
Mamun <mmamun.roshid@gmail.com>
2022-05-23 12:26:36 +0600

    page refresh issue fixing

--- next.config.js
+++ next.config.js
@@ -1,12 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // reactStrictMode: true,
+  reactStrictMode: true,
   ignoreDuringBuilds: true,
   env: {
     baseUrl: "https://node.pickupexpressbd.com/api",
     baseUrlForImages: "",
     youtubeApiKey: "AIzaSyB-erGj8FOrDWqnTP1j4pVY6qP21aVwNf0",
   },
+  optimizeFonts: false,
+  trailingSlash: true,
   images: {
     loader: "imgix",
     path: "https://yunus-centre.netlify.app/",
