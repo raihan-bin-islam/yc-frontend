b2e9cb653a380a23a324f324b5c947a46733a72a
Khandaker Nabiul <na.ninad60@gmail.com>
2022-05-24 15:44:15 +0600

    team member card component created

---
+++ Components/AboutUs/AboutTeam/MemberCard/MemberCard.js
@@ -0,0 +1,37 @@
+import React from "react";
+
+import IconLinkedIn from "../../../../public/linked_in.svg";
+
+// CSS
+import styles from "./MemberCard.module.scss";
+
+const MemberCard = ({ image, title, designation, linkedIn }) => {
+  const {
+    aboutTeamBodyCard,
+    aboutTeamBodyCardImg,
+    aboutTeamBodyCardContent,
+    aboutTeamBodyCardContentContainer,
+    aboutTeamBodyCardLinkedIn,
+  } = styles;
+
+  return (
+    <div className={aboutTeamBodyCard}>
+      <div className={aboutTeamBodyCardImg}>
+        <img src={image} alt={title} />
+      </div>
+      <div className={aboutTeamBodyCardContent}>
+        <div className={aboutTeamBodyCardContentContainer}>
+          <h2>{title}</h2>
+          <h5>{designation}</h5>
+        </div>
+      </div>
+      <div className={aboutTeamBodyCardLinkedIn}>
+        <a href={linkedIn} target="_blank" rel="noreferrer">
+          <img src={IconLinkedIn.src} />
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default MemberCard;
