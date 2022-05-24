c16553d6e5ecb6cff2d308d4b46f97d2bea16a6d
farhan21 <farhanabsar21@gmail.com>
2022-05-24 16:40:00 +0600

    organizasions

--- Components/SocialBusinessOrg/OrgGrid/OrgGrid.js
+++ Components/SocialBusinessOrg/OrgGrid/OrgGrid.js
@@ -1,22 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Pagination from '../../Shared/Pagination/Pagination';
 import styles from './orgGrid.module.scss'
 
-const OrgGrid = ({subHeadline, mainHeadline, orgData}) => {
+const OrgGrid = ({subHeadline, mainHeadline, orgData, countPerPage}) => {
     // styles
-    const {orgGridContainer, orgGridHeader, orgGridBody, orgGridBodyCard} = styles 
+    const {orgGridContainer, orgGridHeader, orgGridBody, orgGridBodyCard, paginateContainer} = styles 
+
+    // paginate
+    const [startOffset, setStartOffset] = useState(1);
+    const [endOffset, setEndOffset] = useState(3);
+    const contentsPerPage = countPerPage;
+
+    const handlePageClick = (e) => {
+        const currentPage = e.selected;
+        setStartOffset(contentsPerPage * currentPage + 1);
+        setEndOffset(contentsPerPage * currentPage + contentsPerPage);
+    }; 
+
     return (
         <section className={orgGridContainer}>
             {/*profit section */}
             <div className={orgGridHeader}>
-                <h3>{subHeadline}</h3>
-                <h2>{mainHeadline}</h2>
+                <h3>{!subHeadline ? '' : subHeadline}</h3>
+                <h2>{!mainHeadline ? '' : mainHeadline}</h2>
             </div>
             <div className={orgGridBody}>
-                {orgData.map((org, index) => 
+                {orgData && orgData.map((org, index) => 
                     <div key={`org_${index}`} className={orgGridBodyCard}>
                         <img src={org.logo} alt="organizations"/>
                     </div>
                 )}
+            </div>
+            <div className={paginateContainer}>
+                <Pagination 
+                    length={orgData.length} 
+                    contentsPerPage={contentsPerPage} 
+                    onPageChange={handlePageClick} 
+                />
             </div>
         </section>
     );
--- Components/SocialBusinessOrg/SocialBusinessOrgBody/SocialBusinessOrgBody.js
+++ Components/SocialBusinessOrg/SocialBusinessOrgBody/SocialBusinessOrgBody.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import OrgGrid from '../OrgGrid/OrgGrid';
 import orgData from '../../../staticPageData/organizations.json'
+import styles from './socialbusinessorgbody.module.scss'
+import sbOrgPath from '../../../public/assets/images/social_business/sb_org_path.png'
 
 const SocialBusinessOrgBody = () => {
+    const { socialBusinessOrgBodyContainer, sb_org_path } = styles
     return (
-        <section>
+        <section className={socialBusinessOrgBodyContainer}>
+            <div className={sb_org_path}>
+                <img src={sbOrgPath.src} alt='sb-org-path'/>
+            </div>
             <div className='container-layout'>
-                <OrgGrid subHeadline='non-profit' mainHeadline='organisations' orgData={orgData}/>
-                <OrgGrid subHeadline='profit' mainHeadline='organisations' orgData={orgData}/>
-                <OrgGrid subHeadline='' mainHeadline='joint ventures' orgData={orgData}/>
+                <OrgGrid 
+                    subHeadline='non-profit' 
+                    mainHeadline='organisations' 
+                    orgData={orgData}
+                    countPerPage={8}
+                />
+                <OrgGrid 
+                    subHeadline='profit' 
+                    mainHeadline='organisations' 
+                    orgData={orgData}
+                    countPerPage={8}
+                />
+                <OrgGrid 
+                    subHeadline='profit' 
+                    mainHeadline='organisations' 
+                    orgData={orgData}
+                    countPerPage={8}
+                />
             </div>
         </section>
     );
