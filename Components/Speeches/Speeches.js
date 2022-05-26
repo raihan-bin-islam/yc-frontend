bb409b65cd6f9ace2475c1ac83139d383b89d8bc
farhan21 <farhanabsar21@gmail.com>
2022-05-26 11:21:07 +0600

    speeches

---
+++ Components/Speeches/Speeches.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './speeches.module.scss'
+
+const Speeches = () => {
+    // classnames
+    const { speechContainer } = styles
+
+
+    return (
+        <section className={speechContainer}>
+            
+        </section>
+    );
+};
+
+export default Speeches;
---
+++ pages/media/speeches/speeches.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const speechesPage = () => {
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default speechesPage;
