24e715b68db4bc7107c0bc3d2100a2fd74182c07
farhan21 <farhanabsar21@gmail.com>
2022-05-26 17:52:50 +0600

    media kit

---
+++ pages/media-kit/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MediaKit from '../../Components/MediaKit/MediaKit';
+
+const mediaKitPage = () => {
+    return (
+        <>
+          <MediaKit/>  
+        </>
+    );
+};
+
+export default mediaKitPage;
--- pages/professor-yunus/speeches/index.js
+++ pages/professor-yunus/speeches/index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Speeches from '../../../Components/Speeches/Speeches';
 
 const speechesPage = () => {
     return (
         <>
-            
+          <Speeches/>  
         </>
     );
 };
