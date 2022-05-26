6c5cf3fd44f8c2b49cfe7aa6e56daa9fdfad5a54
raihan <rishaon0@gmail.com>
2022-05-26 17:05:49 +0600

    recommended readings hero section developed

--- Components/Publications/RecommendedReadings/RecommendedReadings.js
+++ Components/Publications/RecommendedReadings/RecommendedReadings.js
@@ -1,12 +1,36 @@
 import React from "react";
+import HomeLatestEvents from "../../Home/HomeLatestEvents/HomeLatestEvents";
 import HeroBanner from "../../Shared/HeroBanner/HeroBanner";
-import { container, sectionTitle } from "./recommended.module.scss";
+import {
+  container,
+  heroContent,
+  sectionTitle,
+  bookDetailsContainer,
+  bookDetails,
+  author,
+  details,
+} from "./recommended.module.scss";
 
 const RecommendedReadings = () => {
   return (
     <section className={container}>
       <HeroBanner large />
-      <h2 className={sectionTitle}>Recommended Readings</h2>
+      <div className={heroContent}>
+        <h2 className={sectionTitle}>Recommended Readings</h2>
+        <div className={bookDetailsContainer}>
+          <img src="/assets/images/publications/recommended_readings/book.png" alt="" />
+          <div className={bookDetails}>
+            <div>
+              <h3>Building Social Business</h3>
+              <p className={author}>By Professor Muhammad Yunus</p>
+            </div>
+            <p className={details}>
+              The University Press Limited Red Crescent House, Level 6 61 Motijheel C/A, Dhaka-1000 Ph#+88029565441.
+              01917733741 info@uplbooks.com.bd
+            </p>
+          </div>
+        </div>
+      </div>
     </section>
   );
 };
