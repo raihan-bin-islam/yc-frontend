3e3c4a8440fca35aca4830296f718315606747c0
Khandaker Nabiul <na.ninad60@gmail.com>
2022-05-29 18:19:23 +0600

    media photo gallery responsive done

--- Components/MediaPhotoGallery/MediaPhotoGalleryBody/data.js
+++ Components/MediaPhotoGallery/MediaPhotoGalleryBody/data.js
@@ -179,6 +179,141 @@ const galleryData = [
     image: "/assets/images/media/media-gallery/img-9.png",
     title: "The Congressional Gold Medal",
   },
+  {
+    id: 1,
+    image: "/assets/images/media/media-gallery/img-1.png",
+    title: "Nobel Peace Prize",
+  },
+  {
+    id: 2,
+    image: "/assets/images/media/media-gallery/img-2.png",
+    title: "Olympic Laurel",
+  },
+  {
+    id: 3,
+    image: "/assets/images/media/media-gallery/img-3.png",
+    title: "Presidential Medal of Freedom",
+  },
+  {
+    id: 4,
+    image: "/assets/images/media/media-gallery/img-4.png",
+    title: "The Congressional Gold Medal",
+  },
+  {
+    id: 5,
+    image: "/assets/images/media/media-gallery/img-5.png",
+    title: "Notre Dame Award",
+  },
+  {
+    id: 6,
+    image: "/assets/images/media/media-gallery/img-6.png",
+    title: "The Congressional Gold Medal",
+  },
+  {
+    id: 7,
+    image: "/assets/images/media/media-gallery/img-7.png",
+    title: "Presidential Medal of Freedom",
+  },
+  {
+    id: 8,
+    image: "/assets/images/media/media-gallery/img-8.png",
+    title: "Notre Dame Award",
+  },
+  {
+    id: 9,
+    image: "/assets/images/media/media-gallery/img-9.png",
+    title: "The Congressional Gold Medal",
+  },
+  {
+    id: 1,
+    image: "/assets/images/media/media-gallery/img-1.png",
+    title: "Nobel Peace Prize",
+  },
+  {
+    id: 2,
+    image: "/assets/images/media/media-gallery/img-2.png",
+    title: "Olympic Laurel",
+  },
+  {
+    id: 3,
+    image: "/assets/images/media/media-gallery/img-3.png",
+    title: "Presidential Medal of Freedom",
+  },
+  {
+    id: 4,
+    image: "/assets/images/media/media-gallery/img-4.png",
+    title: "The Congressional Gold Medal",
+  },
+  {
+    id: 5,
+    image: "/assets/images/media/media-gallery/img-5.png",
+    title: "Notre Dame Award",
+  },
+  {
+    id: 6,
+    image: "/assets/images/media/media-gallery/img-6.png",
+    title: "The Congressional Gold Medal",
+  },
+  {
+    id: 7,
+    image: "/assets/images/media/media-gallery/img-7.png",
+    title: "Presidential Medal of Freedom",
+  },
+  {
+    id: 8,
+    image: "/assets/images/media/media-gallery/img-8.png",
+    title: "Notre Dame Award",
+  },
+  {
+    id: 9,
+    image: "/assets/images/media/media-gallery/img-9.png",
+    title: "The Congressional Gold Medal",
+  },
+  {
+    id: 1,
+    image: "/assets/images/media/media-gallery/img-1.png",
+    title: "Nobel Peace Prize",
+  },
+  {
+    id: 2,
+    image: "/assets/images/media/media-gallery/img-2.png",
+    title: "Olympic Laurel",
+  },
+  {
+    id: 3,
+    image: "/assets/images/media/media-gallery/img-3.png",
+    title: "Presidential Medal of Freedom",
+  },
+  {
+    id: 4,
+    image: "/assets/images/media/media-gallery/img-4.png",
+    title: "The Congressional Gold Medal",
+  },
+  {
+    id: 5,
+    image: "/assets/images/media/media-gallery/img-5.png",
+    title: "Notre Dame Award",
+  },
+  {
+    id: 6,
+    image: "/assets/images/media/media-gallery/img-6.png",
+    title: "The Congressional Gold Medal",
+  },
+  {
+    id: 7,
+    image: "/assets/images/media/media-gallery/img-7.png",
+    title: "Presidential Medal of Freedom",
+  },
+  {
+    id: 8,
+    image: "/assets/images/media/media-gallery/img-8.png",
+    title: "Notre Dame Award",
+  },
+  {
+    id: 9,
+    image: "/assets/images/media/media-gallery/img-9.png",
+    title: "The Congressional Gold Medal",
+  },
 ];
 
 export default galleryData;
