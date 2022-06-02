763dab9aaa05c6862fb412a431e99abf0a9e49c6
raihan <rishaon0@gmail.com>
2022-06-02 10:35:48 +0600

    speeches page functional component naming issue fix

--- pages/professor-yunus/speeches/index.js
+++ pages/professor-yunus/speeches/index.js
@@ -2,18 +2,14 @@ import React from "react";
 import useFetch from "../../../Components/Hooks/useFetch";
 import VideoSubPage from "../../../Components/Shared/VideoSubPage/VideoSubPage";
 
-const speechesPage = () => {
+const SpeechesPage = () => {
   const [videoIsLoading, videoData] = useFetch("/yunus-speech");
 
   return (
     <>
-      <VideoSubPage
-        heading="speeches"
-        subHeading="By Professor Muhammad Yunus"
-        videoData={videoData}
-      />
+      <VideoSubPage heading="speeches" subHeading="By Professor Muhammad Yunus" videoData={videoData} />
     </>
   );
 };
 
-export default speechesPage;
+export default SpeechesPage;
