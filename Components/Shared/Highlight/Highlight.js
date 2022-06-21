3b6f4f0664eee237b5ba52f8c1f2f2c3168aa235
raihan <rishaon0@gmail.com>
2022-06-21 11:25:11 +0600

    added page animation on news & highlights pages

--- Components/Shared/Highlight/Highlight.js
+++ Components/Shared/Highlight/Highlight.js
@@ -3,15 +3,19 @@ import styles from "./highlight.module.scss";
 
 import Link from "next/link";
 
+import useScrollReveal from "../../Hooks/useScrollReveal";
+
 const Highlight = ({ id, image, description }) => {
   const { highlightContainer, imgContainer, desc } = styles;
 
   const titledUrl = description.toLowerCase().replace(/\s+/g, "-").trim();
 
+  const { revealFromRight } = useScrollReveal("highlight-component");
+
   return (
     <Link href={`/media/news/${id}`} passHref>
       <a className={highlightContainer}>
-        <div className={highlightContainer}>
+        <div className={`${highlightContainer} ${revealFromRight}`}>
           <div className={imgContainer}>
             <img src={image} alt="highlight image" />
           </div>
