a3dc3d62bf39f935e0868c139177ebeba4b71229
Ishfaq Ahmed <ishfaqahmed0837@gmail.com>
2022-06-30 18:22:09 +0600

    changes in config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   reactStrictMode: true,
   ignoreDuringBuilds: true,
   env: {
@@ -13,6 +18,7 @@ const nextConfig = {
     loader: "imgix",
     path: "https://yunus-centre.netlify.app/",
   },
+  webpack5: false,
 
   async redirects() {
     return [
