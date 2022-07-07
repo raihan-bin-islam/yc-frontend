040ab041130b499c39f1a11596179ceed4d7a864
raihan <rishaon0@gmail.com>
2022-07-07 11:57:09 +0600

    search component build error fix

--- Components/SearchResults/SearchResults.js
+++ Components/SearchResults/SearchResults.js
@@ -4,28 +4,26 @@ import HeroBannerSmall from "../Shared/HeroBannerSmall/HeroBannerSmall";
 import SearchResultsComponent from "../Shared/SearchResultsComponent/SearchResultsComponent";
 import { container, pageTitle, heading, noData } from "./searchResults.module.scss";
 const SearchResults = ({ searchResults }) => {
-  const { articles, news, events } = searchResults;
-
   return (
     <section className={container}>
       {/* Articles */}
       <h4 className={heading}>articles</h4>
-      {articles?.map(({ id, title, thumb_image }) => (
+      {searchResults?.articles?.map(({ id, title, thumb_image }) => (
         <SearchResultsComponent key={id} id={id} title={title} image={thumb_image} type="articles" />
       ))}
-      {articles?.length === 0 && <h2 className={noData}>No Data Found</h2>}
+      {searchResults?.articles?.length === 0 && <h2 className={noData}>No Data Found</h2>}
       {/* News */}
       <h4 className={heading}>news</h4>
-      {news?.map(({ id, title, thumb_image }) => (
+      {searchResults?.news?.map(({ id, title, thumb_image }) => (
         <SearchResultsComponent key={id} id={id} title={title} image={thumb_image} type="news" />
       ))}
-      {news?.length === 0 && <h2 className={noData}>No Data Found</h2>}
+      {searchResults?.news?.length === 0 && <h2 className={noData}>No Data Found</h2>}
       {/* Events */}
       <h4 className={heading}>events</h4>
-      {events?.map(({ id, title, thumb_image }) => (
+      {searchResults?.events?.map(({ id, title, thumb_image }) => (
         <SearchResultsComponent key={id} id={id} title={title} image={thumb_image} type="events" />
       ))}
-      {events?.length === 0 && <h2 className={noData}>No Data Found</h2>}
+      {searchResults?.events?.length === 0 && <h2 className={noData}>No Data Found</h2>}
     </section>
   );
 };
